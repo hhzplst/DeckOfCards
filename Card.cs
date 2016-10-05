50e45df7850a337ba04e818d7a9d0805d504a7a8
Ivy Chang <ivycwq@icloud.com>
2016-10-05 14:44:14 -0700

    adding Card class

---
+++ Card.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Deck {
+  public class Card {
+    private char Suit {get;}
+    private int Rank {get;}
+    public Card(char c, int i) {
+      Suit = c;
+      Rank = i;
+    }
+    public override string ToString() {
+      if (Rank == 1) return $"A{Suit}";
+      if (Rank == 11) return $"J{Suit}";
+      if (Rank == 12) return $"Q{Suit}";
+      if (Rank == 13) return $"K{Suit}";
+      else return $"{Rank}{Suit}";
+    }
+  }
+}
